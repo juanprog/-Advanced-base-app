543eb5aa8d2b8a7f8ffd44e549691d3b6fc81dad
Juan Carlos <jcguitar1994@gmail.com>
2021-09-28 21:06:55 -0300

    Add Dart Defines

--- run_driver_test.bat
+++ run_driver_test.bat
@@ -1,3 +1,5 @@
-#!/bin/bash
+cd ..\..\
+.\chromedriver.exe --port=4444
 
-flutter drive --driver=test_driver/integration_test.dart --target=integration_test/complete_app_test.dart
+cd Advanced-Micro-FrontEnds\base_app\
+flutter drive --driver=test_driver/integration_test.dart --target=integration_test/complete_app_test.dart -d web-server
